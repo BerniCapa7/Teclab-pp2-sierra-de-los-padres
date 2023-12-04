be1b6d4af854d77f70938dab5ffe265d22cb7073
BerniCapa7 <149021202+BerniCapa7@users.noreply.github.com>
2023-12-04 19:13:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_f5zjjm1';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
